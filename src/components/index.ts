7885cc6de1f173206a9cd5d02fea3c722509b446
SiravitPhokeed <siravitsasa@gmail.com>
2022-01-30 13:08:36 +0700

    fix: Incorrect imports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,4 @@
 export { default as Button } from "@components/Button";
 export { Search } from "@components/Input";
-export { default as Link } from "@components/Link";
+export { default as LinkButton } from "@components/Link";
 export { default as Navigation, NextNavigation } from "@components/Navigation";
--- src/layouts/index.ts
+++ src/layouts/index.ts
@@ -1,3 +1,2 @@
 export { default as PageLayout } from "@layouts/PageLayout";
-export { default as ListLayout } from "@layouts/ContentLayout";
-export { default as RegularLayout } from "@layouts/ContentLayout";
+export { default as RegularLayout, ListLayout } from "@layouts/ContentLayout";
