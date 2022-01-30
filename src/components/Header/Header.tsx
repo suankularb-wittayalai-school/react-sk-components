357431de8c45aaa6d72e674a8338ca79c2057653
SiravitPhokeed <siravitsasa@gmail.com>
2022-01-30 15:14:59 +0700

    feat: Header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,101 @@
+// Modules
+import React from "react";
+
+// Types
+import { LinkElement as LinkElementType } from "@utils/types/elements";
+
+const HeaderIcons = ({
+  pageIcon,
+  backGoesTo,
+  backIcon,
+  LinkElement,
+}: {
+  pageIcon: JSX.Element | string;
+  backGoesTo: string;
+  backIcon?: JSX.Element | string;
+  LinkElement?: LinkElementType;
+}) => {
+  const BackIconElement = () =>
+    backIcon ? (
+      // If backIcon is a string, render Material Icon of that string
+      typeof backIcon == "string" ? (
+        <i className="icon header__icons__page" translate="no">
+          {backIcon}
+        </i>
+      ) : (
+        // If backIcon is a JSX Element, render it as is
+        backIcon
+      )
+    ) : (
+      // Default
+      <i className="icon header__icons__page" translate="no">
+        arrow_back
+      </i>
+    );
+  return (
+    <div className="header__icons">
+      {LinkElement ? (
+        <LinkElement href={backGoesTo}>
+          <a>
+            <BackIconElement />
+          </a>
+        </LinkElement>
+      ) : (
+        <a href={backGoesTo}>
+          <BackIconElement />
+        </a>
+      )}
+      {typeof pageIcon == "string" ? (
+        <i className="icon header__icons__page" translate="no">
+          {pageIcon}
+        </i>
+      ) : (
+        pageIcon
+      )}
+    </div>
+  );
+};
+
+/**
+ * Header summarises what the page is about in very few words
+ * 
+ * Header requires a background gradient, apply background gradient with a class or a style entry 
+ * @param name.title The name of the page
+ * @param name.subtitle Some more info about the page (i.e. the type of the page)
+ * @param pageIcon The icon that symobolises the page
+ * @param backGoesTo The link in which clicking Back icon will lead to
+ * @param backIcon The Back icon
+ * @param LinkElement The element wrapping the Anchor (i.e. `Link` from `next/link`)
+ */
+const Header = ({
+  name,
+  pageIcon,
+  backGoesTo,
+  backIcon,
+  LinkElement,
+  className,
+  style,
+}: {
+  name: { title: string; subtitle?: string };
+  pageIcon: JSX.Element | string;
+  backGoesTo: string;
+  backIcon?: JSX.Element;
+  LinkElement?: LinkElementType;
+  className?: string;
+  style?: React.CSSProperties;
+}) => (
+  <header className={`header ${className}`} style={style}>
+    <HeaderIcons
+      pageIcon={pageIcon}
+      backGoesTo={backGoesTo}
+      backIcon={backIcon}
+      LinkElement={LinkElement}
+    />
+    <div className="header__text">
+      <h1>{name.title}</h1>
+      {name.subtitle && <p>{name.subtitle}</p>}
+    </div>
+  </header>
+);
+
+export default Header;
--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -16,7 +16,8 @@ export interface LinkProps {
     href: string;
     children: ReactNode;
   }) => JSX.Element;
-  attr: React.AnchorHTMLAttributes<HTMLAnchorElement>
+  style?: React.CSSProperties;
+  attr?: React.AnchorHTMLAttributes<HTMLAnchorElement>
 }
 
 /**
@@ -25,8 +26,9 @@ export interface LinkProps {
  * @param type The type of the button, can be "filled" | "outlined" | "text"
  * @param icon An icon in the form of a JSX Element, will be placed in front of the text
  * @param url The location of the page this Link Button leads to
+ * @param LinkElement The element wrapping the Anchor (i.e. `Link` from `next/link`)
  */
-const LinkButton = ({ name, type, icon, url, LinkElement, attr }: LinkProps) =>
+const LinkButton = ({ name, type, icon, url, LinkElement, style, attr }: LinkProps) =>
   LinkElement ? (
     <LinkElement href={url}>
       <a
@@ -37,6 +39,7 @@ const LinkButton = ({ name, type, icon, url, LinkElement, attr }: LinkProps) =>
             ? "btn--text"
             : "btn--filled"}`
         }
+        style={style}
       >
         {icon}
         <span>{name}</span>
@@ -51,15 +54,15 @@ const LinkButton = ({ name, type, icon, url, LinkElement, attr }: LinkProps) =>
           ? "btn--text"
           : "btn--filled"
       }
-      download={attr.download}
+      download={attr?.download}
       href={url}
-      hrefLang={attr.hrefLang}
-      media={attr.media}
-      ping={attr.ping}
-      rel={attr.rel}
-      target={attr.target}
-      type={attr.type}
-      referrerPolicy={attr.referrerPolicy}
+      hrefLang={attr?.hrefLang}
+      media={attr?.media}
+      ping={attr?.ping}
+      rel={attr?.rel}
+      target={attr?.target}
+      type={attr?.type}
+      referrerPolicy={attr?.referrerPolicy}
     >
       {icon}
       <span>{name}</span>
