2d1db1429cc3daf77a6a13fc351e0f098504b38a
SiravitPhokeed <siravitsasa@gmail.com>
2022-01-30 14:48:09 +0700

    feat: Support HTML attributes on Buttons

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -1,14 +1,22 @@
 // Modules
-import React from "react";
+import React, { ReactNode } from "react";
 
 // Styles
-import "suankularb-components/dist/css/suankularb-components.min.css"
+import "suankularb-components/dist/css/suankularb-components.min.css";
 
 export interface LinkProps {
   name: string;
   type: "filled" | "outlined" | "text";
   icon?: JSX.Element;
   url: string;
+  LinkElement?: ({
+    href,
+    children,
+  }: {
+    href: string;
+    children: ReactNode;
+  }) => JSX.Element;
+  attr: React.AnchorHTMLAttributes<HTMLAnchorElement>
 }
 
 /**
@@ -18,20 +26,44 @@ export interface LinkProps {
  * @param icon An icon in the form of a JSX Element, will be placed in front of the text
  * @param url The location of the page this Link Button leads to
  */
-const LinkButton = ({ name, type, icon, url }: LinkProps) => (
-  <a
-    className={
-      type == "outlined"
-        ? "btn-outlined"
-        : type == "text"
-        ? "btn--text"
-        : "btn--filled"
-    }
-    href={url}
-  >
-    {icon}
-    <span>{name}</span>
-  </a>
-);
+const LinkButton = ({ name, type, icon, url, LinkElement, attr }: LinkProps) =>
+  LinkElement ? (
+    <LinkElement href={url}>
+      <a
+        className={
+          `${type == "outlined"
+            ? "btn-outlined"
+            : type == "text"
+            ? "btn--text"
+            : "btn--filled"}`
+        }
+      >
+        {icon}
+        <span>{name}</span>
+      </a>
+    </LinkElement>
+  ) : (
+    <a
+      className={
+        type == "outlined"
+          ? "btn-outlined"
+          : type == "text"
+          ? "btn--text"
+          : "btn--filled"
+      }
+      download={attr.download}
+      href={url}
+      hrefLang={attr.hrefLang}
+      media={attr.media}
+      ping={attr.ping}
+      rel={attr.rel}
+      target={attr.target}
+      type={attr.type}
+      referrerPolicy={attr.referrerPolicy}
+    >
+      {icon}
+      <span>{name}</span>
+    </a>
+  );
 
 export default LinkButton;
