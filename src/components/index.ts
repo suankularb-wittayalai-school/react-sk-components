dec256fc67d249b354499c8cfa1a27122dcb0bf7
SiravitPhokeed <siravitsasa@gmail.com>
2022-02-01 02:59:14 +0700

    fix: Removed Next Navigation from index

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,4 @@ export { default as Button } from "@components/Button";
 export { default as Header } from "@components/Header";
 export { Search } from "@components/Input";
 export { default as LinkButton } from "@components/Link";
-export { default as Navigation, NextNavigation } from "@components/Navigation";
+export { default as Navigation } from "@components/Navigation";
