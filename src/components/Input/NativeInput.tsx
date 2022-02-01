0e64959a5111530ad62a2634de9a6b3a3cad31b9
SiravitPhokeed <siravitsasa@gmail.com>
2022-02-01 15:57:32 +0700

    refactor: Removed impossible input atrributes

--- src/components/Input/NativeInput.tsx
+++ src/components/Input/NativeInput.tsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 
 export interface NativeInputProps {
   name: string;
-  type: "email" | "number" | "password" | "tel" | "text" | "url";
+  type: "color" | "date" | "datetime-local" | "month" | "time";
   label: string;
   defaultValue?: string | number;
   attr?: React.InputHTMLAttributes<HTMLInputElement>;
@@ -18,7 +18,30 @@ const NativeInput = ({
 }: NativeInputProps): JSX.Element => {
   const [inputValue, setInputValue] = useState(defaultValue);
 
-  return <div className="input--persistent"></div>;
+  return (
+    <div className="input--persistent">
+      <input
+        aria-labelledby={name}
+        onChange={(e) => setInputValue(e.target.value)}
+        autoComplete={attr?.autoComplete}
+        autoFocus={attr?.autoFocus}
+        crossOrigin={attr?.crossOrigin}
+        disabled={attr?.disabled}
+        enterKeyHint={attr?.enterKeyHint}
+        form={attr?.form}
+        list={attr?.list}
+        name={name}
+        placeholder={label}
+        readOnly={attr?.readOnly}
+        required={attr?.required}
+        type={type}
+        value={inputValue}
+      />
+      <label className="input--persistent__label" htmlFor={name}>
+        {label}
+      </label>
+    </div>
+  );
 };
 
 export default NativeInput;
