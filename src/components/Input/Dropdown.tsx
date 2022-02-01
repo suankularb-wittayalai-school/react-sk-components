d646501607f126ae6aa62fc316a1849069996a40
SiravitPhokeed <siravitsasa@gmail.com>
2022-02-01 18:46:09 +0700

    feat: Dropdown functionality

--- src/components/Input/Dropdown.tsx
+++ src/components/Input/Dropdown.tsx
@@ -5,12 +5,24 @@ import React, { useState } from "react";
 import MaterialIcon from "@components/Icon/MaterialIcon";
 
 export interface DropdownProps {
+  name: string;
+  label: string;
   options: Array<{ value: any; label: string }>;
+  placeholder?: string;
+  noOptionsText?: boolean;
   icon?: { expandMore: JSX.Element; expandLess: JSX.Element };
 }
 
-const Dropdown = ({ options, icon }: DropdownProps): JSX.Element => {
+const Dropdown = ({
+  name,
+  label,
+  options,
+  placeholder,
+  noOptionsText,
+  icon,
+}: DropdownProps): JSX.Element => {
   const [showList, setShowList] = useState<boolean>(false);
+  const [hasBeenSelected, setHasBeenSelected] = useState<boolean>(false);
   const [selectedItemValue, setSelectedItemValue] = useState<any>(
     options.length > 0 ? options[0].value : undefined
   );
@@ -22,30 +34,42 @@ const Dropdown = ({ options, icon }: DropdownProps): JSX.Element => {
         aria-haspopup="listbox"
         className="dropdown__button"
         onClick={() => setShowList(!showList)}
-        onBlur={() => setShowList(false)}
         role="combobox"
       >
-        <span>Selected Item</span>
+        <span>
+          {hasBeenSelected || !placeholder
+            ? options.find((option) => selectedItemValue === option.value)
+                ?.label || placeholder
+            : placeholder}
+        </span>
         <div className="dropdown__icon">
-          {showList ? (
-            <MaterialIcon icon="expand_less" />
-          ) : (
-            <MaterialIcon icon="expand_more" />
-          )}
+          {showList
+            ? icon?.expandLess || <MaterialIcon icon="expand_less" />
+            : icon?.expandMore || <MaterialIcon icon="expand_more" />}
         </div>
       </button>
       {showList && (
         <div className="dropdown__options" role="listbox">
           {options.length == 0 ? (
-            <button disabled>No options</button>
+            <button disabled>{noOptionsText || "No options"}</button>
           ) : (
             options.map((option) =>
-              selectedItemValue === option.value ? (
-                <button aria-selected="true" role="option" className="selected">
+              hasBeenSelected && selectedItemValue === option.value ? (
+                <button aria-selected="true" className="selected" role="option">
                   {option.label}
                 </button>
               ) : (
-                <button aria-selected="false" role="option">
+                <button
+                  aria-selected="false"
+                  onClick={() => {
+                    if (!hasBeenSelected) {
+                      setHasBeenSelected(true);
+                    }
+                    setSelectedItemValue(option.value);
+                    setShowList(false);
+                  }}
+                  role="option"
+                >
                   {option.label}
                 </button>
               )
@@ -53,7 +77,9 @@ const Dropdown = ({ options, icon }: DropdownProps): JSX.Element => {
           )}
         </div>
       )}
-      <div className="dropdown__label">Label</div>
+      <label className="dropdown__label" htmlFor={name}>
+        {label}
+      </label>
     </div>
   );
 };
