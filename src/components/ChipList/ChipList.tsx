2fab4b5487f2b82ed4ac8848cb49111e8d8ffc6a
SiravitPhokeed <siravitsasa@gmail.com>
2022-03-10 23:24:41 +0700

    feat: Chip List

--- src/components/ChipList/ChipList.tsx
+++ src/components/ChipList/ChipList.tsx
@@ -1,7 +1,23 @@
-const ChipList = (): JSX.Element => (
-  <div className="chip-list">
-    
-  </div>
-);
+// Modules
+import { ReactNode } from "react";
+
+interface ChipList {
+  scrollable?: boolean;
+  noWrap?: boolean;
+  children: ReactNode;
+}
+
+const ChipList = ({ scrollable, noWrap, children }: ChipList): JSX.Element =>
+  scrollable ? (
+    <div className="x-scroll-content">
+      <div className="x-scroll-content__content">
+        <div className="chip-list--no-wrap">{children}</div>
+      </div>
+    </div>
+  ) : (
+    <div className={noWrap ? "chip-list--no-wrap" : "chip-list"}>
+      {children}
+    </div>
+  );
 
 export default ChipList;
