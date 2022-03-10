941684dedbe64a3d8ba65246f17a121638aff7c8
SiravitPhokeed <siravitsasa@gmail.com>
2022-03-11 04:27:12 +0700

    feat: Filter Chip comment

--- src/components/Chip/FilterChip.tsx
+++ src/components/Chip/FilterChip.tsx
@@ -8,13 +8,23 @@ import MaterialIcon from "../Icon/MaterialIcon";
 // Types
 import { ChipProps } from "./Chip";
 
+/**
+ * A Chip that can be toggled on and off
+ * @param name The text to display inside the Chip
+ * @param appearance How the Chip looks, can be either "regular" (default) or "elevated"
+ * @param selected The default state of this Chip
+ * @param avatar A circular image leading the Chip
+ * @param leadingIcon The Icon to the left of the Chip
+ * @param trailingIcon The Icon to the right of the Chip
+ * @param onClick Triggered when the Chip is clicked
+ */
 const FilterChip = ({
   name,
   appearance,
   selected: value,
+  avatar,
   leadingIcon,
   trailingIcon,
-  avatar,
   className,
   style,
 }: ChipProps) => {
