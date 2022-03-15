9124c1182b19760103e055ab846ab08dcef98367
SiravitPhokeed <siravitsasa@gmail.com>
2022-03-15 22:10:49 +0700

    feat: More attrs in Dialog

--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -3,17 +3,27 @@ import { ReactNode } from "react";
 
 interface DialogProps {
   type: "regular" | "large";
-  children?: ReactNode
+  show?: boolean;
+  hasOverlay?: boolean;
+  children?: ReactNode;
 }
 
 /**
- * Dialog
+ * Dialog presents immediate information and prompts the user to make crucial decisions
+ * @param type `"regular" | "large"`
+ * @param show If Dialog is currently visible
+ * @param hasOverlay If there is a dark scrim in the background when the Dialog is shown
  */
-const Dialog = ({ type, children }: DialogProps): JSX.Element => {
+const Dialog = ({
+  type,
+  show,
+  hasOverlay,
+  children,
+}: DialogProps): JSX.Element => {
   return (
-    <div className="dialog__wrapper">
+    <div className={`dialog__wrapper ${show ? "show" : ""}`}>
       {/* Overlay */}
-      <div className="overlay" />
+      {hasOverlay && <div className="overlay" />}
 
       {/* Dialog */}
       <dialog className={type == "large" ? "dialog--large" : "dialog"}>
