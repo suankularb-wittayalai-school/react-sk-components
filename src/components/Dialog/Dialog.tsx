40204c2082272e8394e670a4d1702db5bbf882bf
SiravitPhokeed <siravitsasa@gmail.com>
2022-03-18 14:39:21 +0700

    doc: Better Dialog comment

--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -33,17 +33,33 @@ export interface DialogProps {
 
 /**
  * Dialog presents information in need of immediate attention and prompts the user to make crucial decisions
+ * 
+ * **General**
  * @param type `"regular" | "large"`
  * @param label The text label for screenreaders
+ * 
+ * **Header**
  * @param title The text to show on top of the Dialog, summarises the content
  * @param icon The hero icon on top of title text, will make header centered
  * @param supportingText A paragraph explaining the title in more detail
  * @param closeIcon On mobile, an icon button closes the Dialog; this icon can be customized here
+ * 
+ * ---
+ * **Actions**
  * @param actions An array of 1-2 action(s), each includes `name`, `type`, and `isDangerous`
+ * 
+ * ---
+ * **State**
  * @param show If Dialog is currently visible
+ * 
+ * ---
+ * **Attrs**
  * @param noOverlay Removes the dark scrim in the background when the Dialog is shown
  * @param noBlurToClose The user cannot click outside Dialog to close it
  * @param isBlank Removes all default components
+ * 
+ * ---
+ * **Callbacks**
  * @param onClose Triggered when button with type `close` or close button is clicked/tapped
  * @param onSubmit Triggered when button with type `submit` is clicked/tapped
  */
