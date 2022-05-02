ddf40db47ec21233e382c791a43289c17ace4079
Siravit Phokeed <siravitsasa@gmail.com>
2022-05-02 21:13:51 +0700

    feat: File Input helper message

--- src/components/Input/index.ts
+++ src/components/Input/index.ts
@@ -1,4 +1,5 @@
 export { default as Dropdown } from "./Dropdown";
+export { default as FileInput } from "./FileInput";
 export { default } from "./KeyboardInput";
 export { default as NativeInput } from "./NativeInput";
 export { default as Search } from "./Search";
