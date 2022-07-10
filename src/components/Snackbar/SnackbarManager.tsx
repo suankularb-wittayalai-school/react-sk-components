4e262893cd55b4d7b396e47de529b84ec759804d
Siravit Phokeed <siravitsasa@gmail.com>
2022-07-10 07:33:42 +0700

    feat: Kind of implemented Snackbar Manager

--- src/components/Snackbar/SnackbarManager.tsx
+++ src/components/Snackbar/SnackbarManager.tsx
@@ -1,6 +1,6 @@
 // Modules
 import { AnimatePresence } from "framer-motion";
-import { useEffect, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 
 // Components
 import Snackbar from "./Snackbar";
@@ -23,95 +23,101 @@ const SnackbarManager = ({
   const [current, setCurrent] = useState<WaitingSnackbar | null>(null);
   const [internalQueue, setInternalQueue] = useState<WaitingSnackbar[]>([]);
 
-  // Utils
-  const wait = (ms: number) =>
-    new Promise((resolve) => setTimeout(resolve, ms));
+  const [goingThroughQueue, setGoingThroughQueue] = useState<boolean>(false);
+  const [accepting, setAccepting] = useState<boolean>(true);
+
+  // (@SiravitPhokeed)
+  // This implementation barely works. It’s almost embarassing for me to put
+  // my name on this. Oh well.
+  // Can someone redo this? Thanks.
+
+  // States: current, itemsServed, internalQueue
+  // 1. Copy `queue` into `internalQueue`
+  // 2. Listen for changes in `internalQueue`
+  //    1. Set `itemsServed` to 0
+  //    2. Loop through `internalQueue`
+  //       1. Set `current` to `internalQueueItem`
+  //       2. Wait 5 seconds
+  //       3. Set `current` to null
+  //       4. Increase `itemsServed` by 1
+  //    3. If `queue`’s length is more than `itemsServed` (haven’t shown everything yet)
+  //       1. Set `internalQueue` to `queue` sliced with `itemsServed` as start
+  //    4. Else
+  //       1. Completely clear `queue`
+
+  useEffect(() => {
+    async function goThroughQueue() {
+      setGoingThroughQueue(true);
+      for (let internalQueueItem of internalQueue) {
+        setCurrent(internalQueueItem);
+        await new Promise((resolve) => setTimeout(resolve, 5000));
+        setCurrent(null);
+      }
+      setGoingThroughQueue(false);
+      setAccepting(true);
+    }
+
+    if (queue.length > 0 && internalQueue.length > 0) goThroughQueue();
+  }, [internalQueue]);
+
+  useEffect(() => {
+    if (accepting) {
+      if (queue.length > internalQueue.length) {
+        setAccepting(false);
+        setInternalQueue(queue.slice(internalQueue.length));
+      } else if (queue.length > 0) {
+        setInternalQueue([])
+        setQueue([]);
+      }
+    } else if (!goingThroughQueue && queue.length > 0) {
+      setInternalQueue(queue);
+    }
+  }, [accepting, queue]);
 
   // DEBUG
-  useEffect(() => {
-    console.log({ queue, internalQueue, current });
-  }, [queue, internalQueue, current]);
+  // useEffect(() => {
+  //   console.log({ queue, goingThroughQueue, internalQueue, current });
+  // }, [queue, goingThroughQueue, internalQueue, current]);
   // END-DEBUG
-
-  // useEffect(() => setInternalQueue(queue), [queue]);
-
-  // useEffect(() => {
-  //   if (queue.length > 0) setCurrent(queue[0]);
-  // }, [queue[0]]);
-
-  // useEffect(() => {
-  //   if (current) {
-  //     const newQueue = internalQueue;
-  //     newQueue.shift();
-  //     setInternalQueue(newQueue);
-
-  //     setTimeout(() => {
-  //       setCurrent(null);
-  //       setQueue(internalQueue);
-  //       if (internalQueue.length > 0) setCurrent(internalQueue[0]);
-  //     }, 6000);
-  //   }
-  // }, [current]);
-
-  useEffect(() => {
-    console.log({ queueInQueue: queue });
-    if (queue.length > 0) {
-      console.log("queue is longer than 0");
-      setCurrent(queue[0]);
-      console.log("current is set to first item of queue");
-    } else setCurrent(null);
-  }, [queue[0]]);
-
-  useEffect(() => {
-    console.log({ queueInCurrent: queue });
-    if (current) {
-      console.log("timer will start");
-      setTimeout(() => {
-        console.log("5 seconds have past");
-        console.log({ queueInTimeOut: queue });
-        console.log({ newQueueShouldBe: queue.slice(1) });
-        setQueue(queue.slice(1));
-        console.log("first queue item is removed");
-      }, 5000);
-    } else {
-      setCurrent(queue[0]);
-    }
-  }, [current]);
-
-  useEffect(() => {
-    console.log({ queueInQueue: queue });
-    setTimeout(() => {
-      console.log({ queueInTimeout: queue });
-      setQueue(queue.slice(1));
-    }, 5000);
-  }, [queue[0]]);
 
   return (
     <aside>
       {/* DEBUG */}
-      <p
+      {/* <p
         style={{
           position: "absolute",
           top: 8,
           left: 8,
           width: 960,
           zIndex: 100,
+          pointerEvents: "none",
+          backdropFilter: "blur(8px)",
         }}
       >
         <strong>Queue</strong>: {JSON.stringify(queue)}
+        <br />
+        <strong>Going through queue</strong>: {goingThroughQueue ? "✅" : "❎"}
+        <br />
+        <strong>Accepting new queue items</strong>: {accepting ? "✅" : "❎"}
+        <br />
+        <strong>Still more to go through</strong>
+        {": "}
+        {queue.length > internalQueue.length
+          ? JSON.stringify(queue.slice(internalQueue.length))
+          : "DONE"}
         <br />
         <strong>Internal queue</strong>: {JSON.stringify(internalQueue)}
         <br />
         <strong>Current</strong>: {JSON.stringify(current)}
         <br />
-      </p>
+      </p> */}
       {/* END-DEBUG */}
 
       <AnimatePresence>
-        {queue[0] && (
+        {current && (
           <Snackbar
-            id={queue[0].id}
-            text={queue[0].text}
+            id={current.id}
+            text={current.text}
             onClose={() => setQueue(queue.slice(1))}
           />
         )}
