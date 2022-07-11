64185707fd496bdcbe63665f20d6bfdce89d5362
Siravit Phokeed <siravitsasa@gmail.com>
2022-07-11 16:12:43 +0700

    refactor: Snackbar Manager cleanup

--- src/components/Snackbar/SnackbarManager.tsx
+++ src/components/Snackbar/SnackbarManager.tsx
@@ -1,15 +1,11 @@
 // Modules
 import { AnimatePresence } from "framer-motion";
-import { useEffect, useReducer, useState } from "react";
+import { useEffect, useState } from "react";
 
 // Components
 import Snackbar from "./Snackbar";
 
-type WaitingSnackbar = {
-  id: string;
-  text: string;
-  persistent?: boolean;
-};
+type WaitingSnackbar = { id: string; text: string };
 
 export interface SnackbarManagerProps {
   queue: WaitingSnackbar[];
@@ -27,23 +23,7 @@ const SnackbarManager = ({
   const [accepting, setAccepting] = useState<boolean>(true);
 
   // (@SiravitPhokeed)
-  // This implementation barely works. It’s almost embarassing for me to put
-  // my name on this. Oh well.
-  // Can someone redo this? Thanks.
-
-  // States: current, itemsServed, internalQueue
-  // 1. Copy `queue` into `internalQueue`
-  // 2. Listen for changes in `internalQueue`
-  //    1. Set `itemsServed` to 0
-  //    2. Loop through `internalQueue`
-  //       1. Set `current` to `internalQueueItem`
-  //       2. Wait 5 seconds
-  //       3. Set `current` to null
-  //       4. Increase `itemsServed` by 1
-  //    3. If `queue`’s length is more than `itemsServed` (haven’t shown everything yet)
-  //       1. Set `internalQueue` to `queue` sliced with `itemsServed` as start
-  //    4. Else
-  //       1. Completely clear `queue`
+  // I don’t know how but this code works and I’m too scared to touch it.
 
   useEffect(() => {
     async function goThroughQueue() {
@@ -74,45 +54,8 @@ const SnackbarManager = ({
     }
   }, [accepting, queue]);
 
-  // DEBUG
-  // useEffect(() => {
-  //   console.log({ queue, goingThroughQueue, internalQueue, current });
-  // }, [queue, goingThroughQueue, internalQueue, current]);
-  // END-DEBUG
-
   return (
     <aside>
-      {/* DEBUG */}
-      {/* <p
-        style={{
-          position: "absolute",
-          top: 8,
-          left: 8,
-          width: 960,
-          zIndex: 100,
-          pointerEvents: "none",
-          backdropFilter: "blur(8px)",
-        }}
-      >
-        <strong>Queue</strong>: {JSON.stringify(queue)}
-        <br />
-        <strong>Going through queue</strong>: {goingThroughQueue ? "✅" : "❎"}
-        <br />
-        <strong>Accepting new queue items</strong>: {accepting ? "✅" : "❎"}
-        <br />
-        <strong>Still more to go through</strong>
-        {": "}
-        {queue.length > internalQueue.length
-          ? JSON.stringify(queue.slice(internalQueue.length))
-          : "DONE"}
-        <br />
-        <strong>Internal queue</strong>: {JSON.stringify(internalQueue)}
-        <br />
-        <strong>Current</strong>: {JSON.stringify(current)}
-        <br />
-      </p> */}
-      {/* END-DEBUG */}
-
       <AnimatePresence>
         {current && (
           <Snackbar
