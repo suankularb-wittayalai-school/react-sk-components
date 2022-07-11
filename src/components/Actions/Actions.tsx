bbd8651170e0871e6670ac229f27f006a40dca80
Siravit Phokeed <siravitsasa@gmail.com>
2022-07-11 23:36:55 +0700

    feat: Actions align options

--- src/components/Actions/Actions.tsx
+++ src/components/Actions/Actions.tsx
@@ -1,15 +1,35 @@
 // Modules
 import { ReactNode } from "react";
+import { classNames } from "../../utils/helpers/elements";
 
 // Types
 import { SKComponent } from "../../utils/types";
 
 export interface ActionsProps extends SKComponent {
+  align?: "left" | "center";
   children: ReactNode;
 }
 
-const Actions = ({ children }: ActionsProps): JSX.Element => (
-  <div className="actions">{children}</div>
+const Actions = ({
+  align,
+  children,
+  className,
+  style,
+}: ActionsProps): JSX.Element => (
+  <div
+    className={classNames([
+      "actions",
+      align == "left"
+        ? "actions--left"
+        : align == "center"
+        ? "actions--center"
+        : "",
+      className,
+    ])}
+    style={style}
+  >
+    {children}
+  </div>
 );
 
 export default Actions;
