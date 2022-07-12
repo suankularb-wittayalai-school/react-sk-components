ef39d815d7ba650f589d1cf05abae1c23c4ab11a
Siravit Phokeed <siravitsasa@gmail.com>
2022-07-12 14:06:33 +0700

    fix: Dependency conflicts

--- src/utils/types/elements.ts
+++ src/utils/types/elements.ts
@@ -4,6 +4,7 @@ export type LinkElement = FC<{
   // (@SiravitPhokeed)
   // next/link is being a stubborn baby and won’t accept the fact that `href` has to be a string
   // so we’re using `any` here. Blame the Vercel people.
+  // P.S. If Vercel is reading this, I apologize, please don’t not hire me.
   href: string | any;
   children: ReactNode;
 }>;
